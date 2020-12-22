fe0913170d5cf175db017dbb2505357451abf8ed
FattahSalleh <fattah.sallehuddin@gmail.com>
2020-12-22 09:54:02 +0800

    OpenCV

--- capture.py
+++ capture.py
@@ -1,16 +1,36 @@
 import cv2, time
 
-# 1. Create an object. Zero for external camera.
+#1. Create an object. Zero for external camera.
 video=cv2.VideoCapture(0, cv2.CAP_DSHOW)
 
-# 3. Create a frame object.
-check, frame = video.read()
+a = 0
 
-print (check)
-print (frame) # Representing image
+while True:
 
+    a = a + 1
 
+    #3. Create a frame object.
+    check, frame = video.read()
 
-# 2. Shutdown the camera.
+    print (check)
+    print (frame) #Representing image
+
+    #6. Coverting to grayscale
+    #gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    #4. Show the frame!
+    cv2.imshow("Frame", frame) #'gray' on 'frame' if grayscale
+
+    #5. For press any key to exit (miliseconds)
+    #cv2.waitKey(0)
+
+    #7. For playing
+    key = cv2.waitKey(1)
+
+    if key == ord('q'):
+        break
+
+#2. Shutdown the camera.
 video.release()
 
+cv2.destroyAllWindows
