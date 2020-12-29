cf7590aba575919c991bf96771e2f0139f6b7254
FattahSalleh <fattah.sallehuddin@gmail.com>
2020-12-30 06:26:25 +0800

    Testing CVUI functions

--- cvuitest.py
+++ cvuitest.py
@@ -8,8 +8,12 @@ cvui.init(WINDOW_NAME)
 frame = np.zeros((700, 1200, 3), np.uint8)
 
 while True:
-    frame[:] = (49, 52, 49)
-    cvui.text(frame, 10, 15, 'Hello world!')
+    frame[:] = (49, 52, 49) #BG Colour
+    cvui.text(frame, 100, 150, 'Hello world!')
+
+    cvui.rect(frame, 60, 10, 130, 90, 0xff0000, 0x00ff00)
+
+    cvui.window(frame, 200, 300, 300, 300, "Video1")
 
     # Show window content
     cvui.imshow(WINDOW_NAME, frame)
