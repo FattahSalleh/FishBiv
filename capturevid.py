eb6ccfd02af37356dd45e2f3a9a0d4b26f2c95da
FattahSalleh <fattah.sallehuddin@gmail.com>
2020-12-30 05:07:53 +0800

    Compiled codes to capture.py

--- capturevid.py
+++ capturevid.py
@@ -1,9 +1,13 @@
 import cv2
 import numpy as np
+import cvui
+
+face_cascade = cv2.CascadeClassifier('haarcascades/haarcascade_frontalface_default.xml')
+
 
 # Create a VideoCapture object and read from input file
 # If the input is the camera, pass 0 instead of the video file name
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture("testface.mp4")
 
 # Check if camera opened successfully
 if (cap.isOpened()== False): 
@@ -13,18 +17,24 @@ if (cap.isOpened()== False):
 while(cap.isOpened()):
   # Capture frame-by-frame
   ret, frame = cap.read()
+
+  gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+  faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+
+  for(x, y, w, h) in faces:
+        frame = cv2.rectangle(gray, (x, y), (x + w, y + h), (255, 0, 0), 2)
+
   if ret == True:
 
     # Display the resulting frame
-    cv2.imshow('Frame',frame)
+    cv2.imshow('Frame', gray)
 
-    # Press Q on keyboard to  exit
-    # if cv2.waitKey(25) & 0xFF == ord('q'):
-    #       break
     key = cv2.waitKey(1)
 
-    if key == ord('q'):
-        break
+    # Press Q on keyboard to  exit
+    if cv2.waitKey(100) & 0xFF == ord('q'):
+      break
 
   # Break the loop
   else: 
